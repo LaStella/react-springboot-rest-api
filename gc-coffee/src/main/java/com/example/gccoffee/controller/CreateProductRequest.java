a99bc7e8a74b4ca1d91c50b555364ff5b881a89a
LaStella <jingyu.ham94@gmail.com>
2023-03-10 12:59:52 +0900

    chore: 프로젝트명 변경으로 인해 클래스 삭제

--- gc-coffee/src/main/java/com/example/gccoffee/controller/CreateProductRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.gccoffee.controller;
-
-import com.example.gccoffee.model.Category;
-
-// DTO
-public record CreateProductRequest(String productName, Category category, long price, String description) {
-}
--- gc-coffee/src/main/java/com/example/gccoffee/model/Category.java
+++
@@ -1,5 +0,0 @@
-package com.example.gccoffee.model;
-
-public enum Category {
-    COFFEBEAN_BEAN_PACKAGE
-}
