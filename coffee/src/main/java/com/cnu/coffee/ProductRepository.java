ad774aa4a1f39c2d817533af4158fff5b6a036fd
LaStella <jingyu.ham94@gmail.com>
2023-04-12 08:19:45 +0900

    refactor: 상품 레포지터리에서 CrudRepository를 JpaRepository로 변경

--- coffee/src/main/java/com/cnu/coffee/ProductRepository.java
+++ coffee/src/main/java/com/cnu/coffee/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.cnu.coffee;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    Iterable<Product> findByNameContaining(String name);
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContaining(String name);
+
+    Product findByProductId(String productId);
 }
