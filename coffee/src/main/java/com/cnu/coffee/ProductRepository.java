201c4366119ef5941950fca314f05ba551c8fcfa
LaStella <jingyu.ham94@gmail.com>
2023-04-27 00:51:28 +0900

    chore: 주문 패키지 분리

--- coffee/src/main/java/com/cnu/coffee/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cnu.coffee;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-    Iterable<Product> findByNameContaining(String name);
-}
